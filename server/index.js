4873918c69e1eee777dc07ad572b1ea7b413f35f
Bsssseep <131932227+Bsssseep@users.noreply.github.com>
2023-07-28 13:09:58 -0700

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', require('../controllers/places'))
 
 app.get('/', (req, res) => {
   res.render('home')
